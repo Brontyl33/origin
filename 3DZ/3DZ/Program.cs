f95082b131595d5f0aac30e8b512f3a5e18a9ba0
Brontyl33 <159293698+Brontyl33@users.noreply.github.com>
2024-02-07 21:48:06 +0300

    3 push

---
+++ 3DZ/3DZ/Program.cs
@@ -0,0 +1,69 @@
+﻿
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /* //первое задание
+        Console.WriteLine("Введите радиус основания");
+        double r = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите размер образующей");
+        double l = Convert.ToDouble(Console.ReadLine());
+        const double pi = 3.141592654;
+        Console.WriteLine(" Площадь поверхности плоского конуса равняется" + " " +  ((r + l) * r * pi));
+        Console.ReadLine();
+        */
+
+
+        /* //второе задание
+       Console.WriteLine("Введите число");
+       int num = Convert.ToInt32(Console.ReadLine());
+
+       if (num%2==0)
+       {
+           Console.WriteLine("Число" + " " + num + " " + "четное");
+       }
+       else
+       {
+           Console.WriteLine("Число" + " " + num + " " + "нечетное");
+       }
+       */
+
+
+        /* //третье задание
+        int x = 14; int y = 1; int z = 5;
+
+        x += y - x++ * z;
+        Console.WriteLine("x равен" + " " + x + " " + "y равен" + " " + y + " " + "z равен" + " " + z + " ");
+        z = --x - y * 5;
+        Console.WriteLine("x равен" + " " + x + " " + "y равен" + " " + y + " " + "z равен" + " " + z + " ");
+        y /= x + 5 % z;
+        Console.WriteLine("x равен" + " " + x + " " + "y равен" + " " + y + " " + "z равен" + " " + z + " ");
+        z = x++ + y * 5;
+        Console.WriteLine("x равен" + " " + x + " " + "y равен" + " " + y + " " + "z равен" + " " + z + " ");
+        x = y - x++ * z;
+        Console.WriteLine("x равен" + " " + x + " " + "y равен" + " " + y + " " + "z равен" + " " + z + " ");
+        Console.ReadLine();
+        */
+
+        /* //четвертое задание
+       int i = default;
+       string s = default;
+       char c = default;
+       bool b = default;
+
+       Console.WriteLine(i);
+       Console.WriteLine(s);
+       Console.WriteLine(c);
+       Console.WriteLine(b);
+       Console.ReadLine();
+        */
+
+        /* //пятое задание
+        Console.WriteLine("Введите рост");
+        int rost = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(nameof(rost) + " " + rost);
+        Console.ReadLine();
+        */
+    }
+}
